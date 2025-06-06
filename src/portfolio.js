e04e0049806111080025e20466e5482be1881ca2
SharkyWiz <asd22u769442@gmail.com>
2025-06-06 13:13:39 +0100

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -33,13 +33,9 @@ const greeting = {
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/sharkywiz",
+  linkedin: "https://www.linkedin.com/in//",
+  gmail: "harrysi5413@gmail.com",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
